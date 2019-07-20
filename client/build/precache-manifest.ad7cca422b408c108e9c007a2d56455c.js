0e63e6ebc6fafd96529e10ea9191fc9bd1d83e46
Sergio Armando Rodriguez Ramirez <sergioardz79@gmail.com>
2019-07-19 22:36:46 -0500

    Deploy to Heroku

---
+++ client/build/precache-manifest.ad7cca422b408c108e9c007a2d56455c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a28e76b8dcab90008700ae72388cdce4",
+    "url": "/static/media/bookshelf.a28e76b8.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c948b0d031c4d5aa54b4",
+    "url": "/static/js/main.a95c3619.chunk.js"
+  },
+  {
+    "revision": "2232c6546eb17263e597",
+    "url": "/static/js/2.b8654b66.chunk.js"
+  },
+  {
+    "revision": "c948b0d031c4d5aa54b4",
+    "url": "/static/css/main.4d341d55.chunk.css"
+  },
+  {
+    "revision": "768e7c462c8d9b06d675a965a51171d0",
+    "url": "/index.html"
+  }
+];
